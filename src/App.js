a07b01fbc10a3565a39b408d80eac07998b0b786
Sai charan bandi <108537114+Charan2409@users.noreply.github.com>
2023-11-16 01:03:08 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import "./styles.css";
+import { useState } from "react";
+import Data from "./Data.json";
+
+export default function App() {
+  const [userList, setUserList] = useState(Data.users);
+  const [searchText, setSearchText] = useState("");
+  const [filteredList, setFilteredList] = useState(userList);
+
+  const handleSearch = () => {
+    let filteredArray = filteredList.filter(
+      (user) =>
+        user.firstName.toLowerCase().includes(searchText) ||
+        user.lastName.toLowerCase().includes(searchText)
+    );
+    setFilteredList(filteredArray);
+  };
+  console.log(searchText);
+  return (
+    <div className="App">
+      <input
+        type="text"
+        onChange={(e) => setSearchText(e.target.value.toLowerCase())}
+      />
+      <button onClick={handleSearch}>Search</button>
+      <br />
+      {filteredList.map((user) => {
+        return (
+          <div className="user_list">
+            <span key={user.id} className="userName">
+              {user.firstName + " " + user.lastName}
+            </span>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
